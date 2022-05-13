8d7c1967d29f209deea52ef4bcc536af1a4fe293
Yura <yurakorabel@gmail.com>
2022-05-13 14:08:04 +0300

    Create empty project

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Welcome to Apteka</h1>
+	<a href="user/user.php">--User--</a>
+	<a href="seller/seller.php">--Seller--</a>
+	<a href="admin/admin.php">--Admin--</a>
+</body>
+</html>
--- user/user.php
+++ user/user.php
@@ -3,9 +3,13 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../assets/css/style.css">
 	<title>Document</title>
 </head>
 <body>
-	<h1>Hello GIT</h1>
+	<input type = "button" onclick = "hello()" value = "Display">
+
+
+	<script src="../assets/js/script.js"></script>
 </body>
 </html>
