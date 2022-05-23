b4ea09aa8e6abc0f15c3741afa8feebdac2e7598
Bohdan Havran <danohavran@gmail.com>
2022-05-24 01:31:24 +0300

    add seller UI

--- seller/seller.php
+++ seller/seller.php
@@ -1,25 +1,64 @@
-<?php
-session_start();
-
-if (!$_SESSION) {
-    header('Location: ../login/log.php');
-}
-elseif ($_SESSION['user']['role'] != 0) {
-    header('Location: ../login/log.php');
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="../assets/css/style.css">
-	<title>Admin</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+	<title>Document</title>
 </head>
 <body>
-	<h1>Welcome to Seller Panel)</h1>
-    <?php
-        var_dump($_SESSION['user']);
-    ?>
+
+	<div id="header">
+		<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 p-3 border-bottom">
+	      <a href="../index.php" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+	      	<img src="../assets/img/3248924.png" class="bi me-2" width="40" height="40">
+	      	<h1 aria-label="Bootstrap">Аптека</h1>
+	      </a>
+	    </header>
+	</div>	
+    
+    <main>
+    	<div>
+	    	<div class="center log" id="midle">
+	    		<div class="list-group" style="margin-bottom: 65px;">
+	    			<div class="list-group-item"> 
+	    				<p style="margin-right: 45px; margin-bottom: 0px;" class="btn btn-toggle collapsed p-0 d-inline-flex align-items-center" data-bs-target="#description" data-bs-toggle="collapse" aria-expanded="false">Замовлення №1</p>
+	    				<button type="button" class="btn btn-primary" onclick="location.href='vendor/select_drug_list.php';">Редагувати</button>
+	    				<button type="button" class="btn btn-success" onclick="location.href='vendor/select_drug_list.php';">Підтвердити</button>
+	    				<div class="collapse" id="description">
+				          Привіт
+				        </div>
+	    			</div>
+	    
+	    			<button type="button" class="btn btn-primary mt-3" onclick="location.href='../user/user.php';">Додати</button>
+				</div>
+	    	</div>
+		</div>
+    </main>
+    
+
+    
+    <div id="footer" class="footer1">
+		<footer id="footer" class="d-flex flex-wrap justify-content-between align-items-center py-3 mb-4 border-top">
+		    <div class="col-md-4 d-flex align-items-center">
+		      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+		        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+		      </a>
+		      <span class="mb-3 mb-md-0 text-muted">© 2022 Company, Inc</span>
+		    </div>
+
+		    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+		      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+		      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+		      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+		    </ul>
+		</footer>
+	</div>
+	<script type="text/javascript">
+	   document.getElementById('midle').style.maxHeight = document.documentElement.scrollHeight - document.getElementById('header').scrollHeight - document.getElementById('footer').scrollHeight - 71 + 'px';
+	</script>
+	<script src="../assets/js/script.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 </html>
